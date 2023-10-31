769e49d5a706e78f758afde36248d027b40fdb56
Qing_WANG <50736751+enscm@users.noreply.github.com>
2023-10-31 17:00:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ from setuptools import setup
 
 setup(
     name='ChIMES_Analysis',
-    version='0.1',
+    author='Qing WANG',
+    author_email='qing.wang@outlook.fr',
+    version='0.1.0',
     packages=['modules'],
     scripts=['bin/gen_frame'],
-)
+    classifiers=[
+        'Programming Language :: Python',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering'],
+    keywords = ['ChIMES', 'Many-Body Potentials', 'Optimization'])
