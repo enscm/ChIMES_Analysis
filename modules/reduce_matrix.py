96a0c641e94c8ee95634a9e0196b6decef120ded
Qing_WANG <50736751+enscm@users.noreply.github.com>
2023-12-05 13:55:13 +0100

    Update reduce_matrix.py

--- modules/reduce_matrix.py
+++ modules/reduce_matrix.py
@@ -3,8 +3,8 @@ import sys
 import numpy as np
 
 def _read_matrix(structure):
-    aa = np.loadtxt(structure+"A.txt",ndmin=1)
-    bb = np.loadtxt(structure+"b.txt",ndmin=0)
+    aa = np.loadtxt(structure+"A.txt",dtype='float')
+    bb = np.loadtxt(structure+"b.txt",dtype='float')
     os.remove("A.txt")
     os.remove("b.txt")
     return aa, bb
@@ -17,13 +17,8 @@ def _reduce_matrix(prefix=None,structure_list=None):
     for line in lines:
         structure = line.strip().split()[0]
         aa, bb = _read_matrix(structure)
-        with open(structure+'/b-labeled.txt', 'r') as f:
-            b_lines = f.readlines()
-            for i, b_line in enumerate(b_lines, 0):
-                if b_line.startswith('+1'):
-                    keep_lines.append(i)
-                    A = aa[keep_lines]
-                    b = bb[keep_lines]
+        A = aa[-1]
+        b = bb[-1]
         np.savetxt(structure + '/' + prefix + 'A.txt', A, delimiter='\t')
         np.savetxt(structure + '/' + prefix + 'b.txt', b, delimiter='\t')
 
